be2d3721f841a1f4e501becc4da016a394fa73b6
Тананина Ирина Михайловна <imitananina@sberbank.ru>
2022-11-18 00:34:17 +0300

    explosion draft

--- src/components/RocketAnimated/RocketAnimated.tsx
+++ src/components/RocketAnimated/RocketAnimated.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import rocket from "../../assets/images/rocket.png";
+import startExplosion from "../../assets/gifs/start-explosion.gif";
 import { useSpring, animated } from 'react-spring'
 import classes from './RocketAnimated.module.scss';
 
@@ -15,7 +16,7 @@ export const RocketAnimated: FC<RocketAnimatedProps> = ({
   onAnimationEnd = () => {}
 }) => {
 
-    const styles = useSpring({
+    const rocketStyles = useSpring({
         from: {transform: "translateY(0px)"},
         //to: [{transform: "translateY(0px}"}],
         to: [{transform: `${'translateY(' + (isAction ? '50px' : '0px') + ')'}`},
@@ -24,13 +25,29 @@ export const RocketAnimated: FC<RocketAnimatedProps> = ({
         onRest: () => { onAnimationEnd() }
     })
 
+    // const explosionStyles = useSpring({
+    //     from: {opacity: 0, transform: "translateY(0px)"},
+    //     to: [{opacity: 1, transform: `${'translateY(' + (isAction ? '50px' : '0px') + ')'}`},
+    //         {opacity: 0}],
+    //     config: {mass: 6, duration: 500},
+    // })
+
     return (
-        <animated.div className={classes.rocketMain} style={{...styles}}>
-            <img
-                src={rocket}
-                alt='HeyHouston Rocket'
-                onClick={handleClick}
-            />
-        </animated.div>
+        <div className={classes.rocketContainer}>
+            <animated.div className={classes.rocketMain} style={{...rocketStyles}}>
+                <img
+                    src={rocket}
+                    alt='HeyHouston Rocket'
+                    onClick={handleClick}
+                />
+            </animated.div>
+
+            {/*<animated.div className={classes.startExplosion} style={{...explosionStyles}}>*/}
+            {/*    <img*/}
+            {/*        src={startExplosion}*/}
+            {/*    />*/}
+            {/*</animated.div>*/}
+        </div>
+
     );
 };
